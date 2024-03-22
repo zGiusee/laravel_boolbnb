86b4994cdc3bd91252d598bad78a14e79a1d0ab7
zGiusee <giusebelforte@gmail.com>
2024-03-22 10:43:13 +0100

    Adjusted buttons class

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-12 d-flex justify-content-between align-items-center my-2 px-3">
-        <p><strong>Create yours: </strong></p>
-        <a class="my_button" href="{{ route('user.apartment.create') }}"><i class="fa-solid fa-plus"></i></a>
-    </div>
     <div class="container-fluid">
         <div class="row">
 
+            {{-- TITLE --}}
             <div class="col-12">
-                <h1 class="my-title">Your Apartments</h1>
+                <h1 class="my-title p-3">Your Apartments</h1>
             </div>
+
+            {{-- ADDING BUTTONS --}}
+            <div class="col-12">
+                <div class="p-3">
+                    <span>New Apartment</span>
+                    <a class="my-btn-sm" href="{{ route('user.apartment.create') }}"><i class="fa-solid fa-plus"></i></a>
+                </div>
+            </div>
+
+            {{-- TABLE --}}
             <div class="col-12">
                 <table class="my-table">
                     <tr>
@@ -30,13 +37,13 @@
                         <td>Francisco Chang</td>
                         <td>Francisco Chang</td>
                         <td class="d-flex justify-content-center align-items-center">
-                            <a href="">
+                            <a class="my-a-sm" href="">
                                 <i class="fa-solid fa-circle-info"></i>
                             </a>
-                            <a href="">
+                            <a class="my-a-sm" href="">
                                 <i class="fa-solid fa-pen-to-square"></i>
                             </a>
-                            <button>
+                            <button class="my-btn-sm">
                                 <i class="fa-regular fa-trash-can"></i>
                             </button>
                         </td>
@@ -49,13 +56,13 @@
                         <td>Centro comercial Moctezuma</td>
                         <td>Francisco Chang</td>
                         <td class="d-flex justify-content-center align-items-center">
-                            <a href="">
+                            <a class="my-a-sm" href="">
                                 <i class="fa-solid fa-circle-info"></i>
                             </a>
-                            <a href="">
+                            <a class="my-a-sm" href="">
                                 <i class="fa-solid fa-pen-to-square"></i>
                             </a>
-                            <button>
+                            <button class="my-btn-sm">
                                 <i class="fa-regular fa-trash-can"></i>
                             </button>
                         </td>
@@ -65,5 +72,4 @@
             </div>
 
         </div>
-        index
     @endsection
