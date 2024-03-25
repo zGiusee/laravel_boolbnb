79923a0254759fa2b09e45699389cfb3ef951abc
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-03-25 09:55:22 +0100

    scss and index

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -1,69 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="col-12 d-flex justify-content-between align-items-center my-2 px-3">
-        <p><strong>Create yours: </strong></p>
-        <a class="my_button" href="{{ route('user.apartment.create') }}"><i class="fa-solid fa-plus"></i></a>
-    </div>
     <div class="container-fluid">
         <div class="row">
 
+            {{-- TITLE --}}
             <div class="col-12">
-                <h1 class="my-title">Your Apartments</h1>
+                <h1 class="my-title p-3">Your Apartments</h1>
             </div>
+
+            {{-- ADDING BUTTONS --}}
+            <div class="col-12">
+                <div class="p-3">
+                    <span>New Apartment</span>
+                    <a class="my-btn-sm" href="{{ route('user.apartment.create') }}"><i class="fa-solid fa-plus"></i></a>
+                </div>
+            </div>
+
+            {{-- TABLE --}}
             <div class="col-12">
                 <table class="my-table">
-                    <tr>
-                        <th>Id</th>
-                        <th>Visible</th>
-                        <th>Title</th>
-                        <th>Address</th>
-                        <th>Square meters</th>
-                        <th>Slug</th>
-                        <th>Tools</th>
-                    </tr>
-                    <tr>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Francisco Chang</td>
-                        <td class="d-flex justify-content-center align-items-center">
-                            <a href="">
-                                <i class="fa-solid fa-circle-info"></i>
-                            </a>
-                            <a href="">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                            </a>
-                            <button>
-                                <i class="fa-regular fa-trash-can"></i>
-                            </button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Francisco Chang</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td class="d-flex justify-content-center align-items-center">
-                            <a href="">
-                                <i class="fa-solid fa-circle-info"></i>
-                            </a>
-                            <a href="">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                            </a>
-                            <button>
-                                <i class="fa-regular fa-trash-can"></i>
-                            </button>
-                        </td>
-                    </tr>
+
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Visible</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Square meters</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Tools</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($apartments as $apartment)
+                            <tr>
+                                <th scope="row">{{ $apartment->user_id }}</th>
+                                <td>{{ $apartment->visible }}</td>
+                                <td>{{ $apartment->title }}</td>
+                                <td>{{ $apartment->address }}</td>
+                                <td>{{ $apartment->square_meters }}</td>
+                                <td>{{ $apartment->slug }}</td>
+                                <td class="d-flex justify-content-center align-items-center">
+                                    <a class="my-a-sm" href="{{ route('user.apartment.show', ['apartment' => $apartment->slug]) }}">
+                                        <i class="fa-solid fa-circle-info"></i>
+                                    </a>
+                                    <a class="my-a-sm" href="">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
+                                    <button class="my-btn-sm">
+                                        <i class="fa-regular fa-trash-can"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
 
                 </table>
             </div>
 
         </div>
-        index
-    @endsection
+    </div>
+@endsection
