81aa33b4010d5911b625aaa706487fdd0c319e09
zGiusee <giusebelforte@gmail.com>
2024-03-26 13:32:41 +0100

    Service seeder done

--- database/migrations/2024_03_21_113057_create_services_table.php
+++ database/migrations/2024_03_21_113057_create_services_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            $table->string('icon', 255);
             $table->timestamps();
         });
     }
--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,8 +13,67 @@ class ServiceSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
+        $services = [
+            [
+                'name' => 'Parking',
+                'icon' => 'fa-solid fa-square-parking',
+            ],
+            [
+                'name' => 'Wi-Fi',
+                'icon' => 'fa-solid fa-wifi',
+            ],
+            [
+                'name' => 'Bathroom',
+                'icon' => 'fa-solid fa-bath',
+            ],
+            [
+                'name' => 'Pool',
+                'icon' => 'fa-solid fa-person-swimming',
+            ],
+            [
+                'name' => 'Elevator',
+                'icon' => 'fa-solid fa-elevator',
+            ],
+            [
+                'name' => 'Facilities For Disabled Guests',
+                'icon' => 'fa-brands fa-accessible-icon',
+            ],
+            [
+                'name' => 'Shower',
+                'icon' => 'fa-solid fa-shower',
+            ],
+            [
+                'name' => 'Heating',
+                'icon' => 'fa-solid fa-fire',
+            ],
+            [
+                'name' => 'Dishwhasher',
+                'icon' => 'fa-solid fa-pump-soap',
+            ],
+            [
+                'name' => 'Tv',
+                'icon' => 'fa-solid fa-tv',
+            ],
+            [
+                'name' => 'Kitchen',
+                'icon' => 'fa-solid fa-kitchen-set',
+            ],
+            [
+                'name' => 'Air Conditioning',
+                'icon' => 'fa-solid fa-fan',
+            ],
+        ];
+
+        foreach ($services as $service) {
+
+            $new_service = new Service();
+            $new_service->name = $service['name'];
+            $new_service->icon = $service['icon'];
+            $new_service->save();
+        }
         //
     }
 }
