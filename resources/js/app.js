9c7630c45e00ec2aaecd555914917ce0f00e270a
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-03-28 00:07:59 +0100

    added front validation

--- resources/js/app.js
+++ resources/js/app.js
@@ -88,3 +88,117 @@ if (old != "") {
     // Assegno all'input il valore del vecchio indirizzo
     address.value = ttSearchBox.setValue(old);
 }
+
+// Validation
+    $(document).ready(function() {
+        // Function to display error message for a specific field
+        function showError(field, message) {
+            $('#' + field + '-error').text(message).show();
+        }
+
+        // Function to hide error message for a specific field
+        function hideError(field) {
+            $('#' + field + '-error').hide();
+        }
+
+        // Function to validate the form
+        function validateForm() {
+            var isValid = true;
+
+            // Validate Title
+            var title = $('#title').val();
+            if (!title) {
+                showError('title', 'The title is required!');
+                isValid = false;
+            } else if (title.length > 100) {
+                showError('title', 'The title must be a maximum of 100 characters long!');
+                isValid = false;
+            } else {
+                hideError('title');
+            }
+
+            // Validate Rooms
+            var rooms = $('#rooms').val();
+            if (!rooms) {
+                showError('rooms', 'The number of rooms is required!');
+                isValid = false;
+            } else if (!$.isNumeric(rooms) || parseInt(rooms) != rooms) {
+                showError('rooms', 'Rooms must be a whole number!');
+                isValid = false;
+            } else {
+                hideError('rooms');
+            }
+
+            // Validate Beds
+            var beds = $('#beds').val();
+            if (!beds) {
+                showError('beds', 'The number of beds is required!');
+                isValid = false;
+            } else if (!$.isNumeric(beds) || parseInt(beds) != beds) {
+                showError('beds', 'Beds must be a whole number!');
+                isValid = false;
+            } else {
+                hideError('beds');
+            }
+
+            // Validate Bathrooms
+            var bathrooms = $('#bathrooms').val();
+            if (!bathrooms) {
+                showError('bathrooms', 'The number of bathrooms is required!');
+                isValid = false;
+            } else if (!$.isNumeric(bathrooms) || parseInt(bathrooms) != bathrooms) {
+                showError('bathrooms', 'Bathrooms must be a whole number!');
+                isValid = false;
+            } else {
+                hideError('bathrooms');
+            }
+
+            // Validate Square Meters
+            var squareMeters = $('#square_meters').val();
+            if (!squareMeters) {
+                showError('square_meters', 'Square meters are required!');
+                isValid = false;
+            } else if (squareMeters.length > 15) {
+                showError('square_meters', 'Square meters must be a maximum of 15 characters long!');
+                isValid = false;
+            } else {
+                hideError('square_meters');
+            }
+
+            // Validate Address
+            var address = $('#address').val();
+            if (!address) {
+                showError('address', 'The address is required!');
+                isValid = false;
+            } else if (address.length > 100) {
+                showError('address', 'The address must be a maximum of 100 characters long!');
+                isValid = false;
+            } else {
+                hideError('address');
+            }
+
+            // Validate Cover Image
+            var coverImg = $('#cover_img').val();
+            if (!coverImg) {
+                showError('cover_img', 'The cover image is required!');
+                isValid = false;
+            } else {
+                var extension = coverImg.split('.').pop().toLowerCase();
+                if (['jpg', 'jpeg', 'png'].indexOf(extension) == -1) {
+                    showError('cover_img', 'The cover image must be in .jpg, .png, or .jpeg format!');
+                    isValid = false;
+                } else {
+                    hideError('cover_img');
+                }
+            }
+
+            return isValid;
+        }
+
+        // On form submit, validate the form
+        $('#submitCreate').click(function(event) {
+            if (!validateForm()) {
+                event.preventDefault(); // Prevent form submission if validation fails
+            }
+        });
+    });
