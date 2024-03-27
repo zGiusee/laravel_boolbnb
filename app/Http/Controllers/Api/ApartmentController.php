e898ac1d6a7c183e426e121704ca82e8d4ed0700
anto24s96 <anto24s96@gmail.com>
2024-03-27 18:58:06 +0100

    [UPDATE] added some changes

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ApartmentController extends Controller
 {
-    //
+    public function index()
+    {
+        $apartments = Apartment::paginate(12);
+
+
+        if (!empty($apartments)) {
+            return response()->json([
+                'success' => true,
+                'results' => $apartments
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => $apartments
+            ]);
+        }
+    }
+
+    public function show($slug)
+    {
+        // Cerca il progetto nel database utilizzando lo slug fornito come parametro
+        $apartment = Apartment::with('services')->where('slug', $slug)->first();
+
+        // Restituisce una risposta JSON con il progetto trovato (se presente) e una flag di successo impostata su true
+        return response()->json([
+            'success'   => true,
+            'apartment' => $apartment
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ApartmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/apartments', [ApartmentController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
