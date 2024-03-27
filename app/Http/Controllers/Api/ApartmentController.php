1de93bd7880bbdff294d89fe9b000e0784d0050f
zGiusee <giusebelforte@gmail.com>
2024-03-27 19:05:38 +0100

    Solved double sentece error

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
-use App\Models\Apartment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\DB;
 
 class ApartmentController extends Controller
@@ -75,6 +73,34 @@ class ApartmentController extends Controller
         return response()->json([
             'succes' => true,
             'results' => $apartments,
+        ]);
+    }
+    public function index()
+    {
+        $apartments = Apartment::paginate(12);
+
+
+        if (!empty($apartments)) {
+            return response()->json([
+                'success' => true,
+                'results' => $apartments
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => $apartments
+            ]);
+        }
+    }
+    public function show($slug)
+    {
+        // Cerca il progetto nel database utilizzando lo slug fornito come parametro
+        $apartment = Apartment::with('services')->where('slug', $slug)->first();
+
+        // Restituisce una risposta JSON con il progetto trovato (se presente) e una flag di successo impostata su true
+        return response()->json([
+            'success'   => true,
+            'apartment' => $apartment
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\ApartmentController;
-use App\Models\Apartment;
-use App\Http\Controllers\Api\ApartmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
