1e135de5d5f049f3f95532c370c684e813245126
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-03-28 03:18:22 +0100

    fix dashboard design

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,15 +2,18 @@
 
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
+    {{-- <h2 class="fs-4 d-flex justify-content-center my-4 my-blue ">
         {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    </h2> --}}
+    <div class="row justify-content-center align-items-center height-100">
+        <div class="col-12 d-flex justify-content-center ">
+            <div class="d-flex justify-content-center my-blue">
+                {{-- <h2 class="">{{ __('Dashboard') }}</h2> --}}
+            </div>
+            <div class=" w-25">
+                {{-- <div class="card-header">{{ __('User Dashboard') }}</div> --}}
 
-                <div class="card-body">
+                <div class="py-3 px-3 d-flex justify-content-center my-blue text-uppercase fs-4 border-bottom border-info  ">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
