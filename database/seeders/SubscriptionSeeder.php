02b43427fd988021f3bce785812ee481d2feedac
moonymel <musumeci.me@gmail.com>
2024-03-29 12:25:21 +0100

    [UPDATE] SubscriptionSeeder

--- database/seeders/SubscriptionSeeder.php
+++ database/seeders/SubscriptionSeeder.php
@@ -14,6 +14,32 @@ class SubscriptionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $subscriptions = [
+            [
+                'name' => 'Base Subscription',
+                'price' => 2.99,
+                'duration' => '24'
+            ],
+            [
+                'name' => 'Medium Subscription',
+                'price' => 5.99,
+                'duration' => '72'
+            ],
+            [
+                'name' => 'Premium Subscription',
+                'price' => 9.99,
+                'duration' => '144'
+            ],
+        ];
+
+        foreach ($subscriptions as $subscription) {
+            $new_subscription = new Subscription();
+
+            $new_subscription->name = $subscription['name'];
+            $new_subscription->price = $subscription['price'];
+            $new_subscription->duration = $subscription['duration'];
+
+            $new_subscription->save();
+        };
     }
 }
