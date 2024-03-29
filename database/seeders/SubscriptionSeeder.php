db871a9dadc2466551e58354aa0fd2f488c50d15
moonymel <musumeci.me@gmail.com>
2024-03-29 12:29:20 +0100

    [UPDATE] correction

--- database/seeders/SubscriptionSeeder.php
+++ database/seeders/SubscriptionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Subscription;
+
 
 class SubscriptionSeeder extends Seeder
 {
