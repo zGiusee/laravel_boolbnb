473a329bdc1aa65b2078a1d746b45fd91686adae
zGiusee <giusebelforte@gmail.com>
2024-03-30 10:53:12 +0100

    Added user on show

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -130,11 +131,16 @@ class ApartmentController extends Controller
     {
         // Cerca il progetto nel database utilizzando lo slug fornito come parametro
         $apartment = Apartment::with('services')->where('slug', $slug)->first();
+        $user_id = $apartment['user_id'];
+        $user = User::where('id', $user_id)
+            ->select('users.name', 'users.surname')
+            ->get();
 
         // Restituisce una risposta JSON con il progetto trovato (se presente) e una flag di successo impostata su true
         return response()->json([
             'success'   => true,
-            'apartment' => $apartment
+            'apartment' => $apartment,
+            'user' => $user,
         ]);
     }
 }
