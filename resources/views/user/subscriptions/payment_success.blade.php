b8afe36f21ef9b51fca6605ad61151f8c983822f
zGiusee <giusebelforte@gmail.com>
2024-04-08 11:11:39 +0200

    Modified thank you

--- resources/views/user/subscriptions/payment_success.blade.php
+++ resources/views/user/subscriptions/payment_success.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="p-4">
+        <a class="my-btn-sm" href="{{ route('user.apartment.index') }}"><i class="fas fa-arrow-left">
+            </i></a>
+    </div>
     <div class="container">
-        <div class="row justify-content-center align-items-center height-100">
+        <div class="row justify-content-center align-items-center">
             <div class="col-12 d-flex justify-content-center ">
-                <div class="d-flex justify-content-center my-blue">
-                </div>
                 <div class="">
                     <div
                         class="py-3 px-3 d-flex justify-content-center my-blue text-uppercase fs-4 border-bottom border-info  ">
