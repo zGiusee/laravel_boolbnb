1dffcca6a235dbe7ee96d4fada97760c4ea6bcd0
moonymel <musumeci.me@gmail.com>
2024-04-08 11:02:51 +0200

    [update] fixed payment success page

--- resources/views/user/subscriptions/payment_success.blade.php
+++ resources/views/user/subscriptions/payment_success.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>
+<div class="container">
+    <div class="row justify-content-center align-items-center height-100">
+        <div class="col-12 d-flex justify-content-center ">
+            <div class="d-flex justify-content-center my-blue">
+            </div>
+            <div class="">
+                <div
+                    class="py-3 px-3 d-flex justify-content-center my-blue text-uppercase fs-4 border-bottom border-info  ">
                     {{ $message }}
-                </h1>
+                </div>
             </div>
         </div>
     </div>
-    </div>
+</div>
 @endsection
