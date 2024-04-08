6fc3969b662f188e03902370155d34aef5cb3d34
moonymel <musumeci.me@gmail.com>
2024-04-08 13:31:14 +0200

    [UPDATE] styled paymend_success and payment_failed

--- resources/views/user/subscriptions/payment_success.blade.php
+++ resources/views/user/subscriptions/payment_success.blade.php
@@ -1,20 +1,27 @@
 @extends('layouts.app')
 
-@section('content')
-    <div class="p-4">
-        <a class="my-btn-sm" href="{{ route('user.apartment.index') }}"><i class="fas fa-arrow-left">
-            </i></a>
-    </div>
-    <div class="container">
-        <div class="row justify-content-center align-items-center">
-            <div class="col-12 d-flex justify-content-center ">
-                <div class="">
-                    <div
-                        class="py-3 px-3 d-flex justify-content-center my-blue text-uppercase fs-4 border-bottom border-info  ">
-                        {{ $message }}
-                    </div>
+@section('content') 
+
+
+<div class="container-fluid position-relative my-success-container p-0 m-0 overflow-y-hidden">
+
+        {{-- BACK BUTTON  --}}
+        <div class="back-button p-0 m-0">
+            <a class="my-btn-sm" href="{{ route('user.apartment.index') }}"><i class="fas fa-arrow-left">
+                </i></a>
+        </div>
+
+        {{-- INFO SECTION  --}}
+        <div class="row justify-content-center align-items-center height-100 p-0 m-0">
+            <div class="col-12 d-flex justify-content-center">
+                <div class="d-flex justify-content-center my-blue">
                 </div>
+                    <div class="d-flex justify-content-center my-blue text-uppercase fs-4 border-bottom border-info">
+                    {{ $message }}
+                    </div>
             </div>
         </div>
-    </div>
+</div>
+
+    
 @endsection
